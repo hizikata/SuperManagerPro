2f68fcc7007dd0f141d93ee749935dc051223367
xiaozhong xu <xxz0528@outlook.com>
2018-05-03 18:25:33 +0800

    MainWindow 180503
    MainWindow 180503

--- SuperManagerPro/ViewModel/MainViewModel.cs
+++ SuperManagerPro/ViewModel/MainViewModel.cs
@@ -1,4 +1,7 @@
 using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
 
 namespace SuperManagerPro.ViewModel
 {
@@ -16,19 +19,75 @@ namespace SuperManagerPro.ViewModel
     /// </summary>
     public class MainViewModel : ViewModelBase
     {
-        /// <summary>
-        /// Initializes a new instance of the MainViewModel class.
-        /// </summary>
+        #region Fields
+        #endregion
+        #region Properties
+        private bool _showMerchandiseManagement;
+
+        public bool ShowMerchandiseManagement
+        {
+            get { return _showMerchandiseManagement; }
+            set
+            {
+                _showMerchandiseManagement = !_showMerchandiseManagement;
+                RaisePropertyChanged(() => ShowMerchandiseManagement);
+            }
+        }
+
+        #endregion
+        #region Constructors
         public MainViewModel()
         {
-            ////if (IsInDesignMode)
-            ////{
-            ////    // Code runs in Blend --> create design time data.
-            ////}
-            ////else
-            ////{
-            ////    // Code runs "for real"
-            ////}
+
         }
+        #endregion
+        #region Commands
+        public ReadOnlyCollection<CommandViewModel> MerchandiseManagementCommand
+        {
+            get
+            {
+                List<CommandViewModel> list = new List<CommandViewModel>()
+                {
+                    new CommandViewModel(new RelayCommand<object>(p=>MerchandiseManagement()),"商品管理")
+                };
+                return new ReadOnlyCollection<CommandViewModel>(list);
+            }
+        }
+        public ReadOnlyCollection<CommandViewModel> MerchandiseManagementCommands
+        {
+            get
+            {
+                List<CommandViewModel> list = new List<CommandViewModel>()
+                {
+                    new CommandViewModel(new RelayCommand<object>(p=>SuppliserManagement()),"供应商信息"),
+                    new CommandViewModel(new RelayCommand<object>(p=>MerchandiseInfo()),"商品信息"),
+                    new CommandViewModel(new RelayCommand<object>(p=>InventoryManagement()),"库存信息")
+                };
+                return new ReadOnlyCollection<CommandViewModel>(list);
+
+            }
+        }
+        #endregion
+        #region Command Methods
+        //改变子菜单的可见性
+        void MerchandiseManagement()
+        {
+            this.ShowMerchandiseManagement = !ShowMerchandiseManagement;
+        }
+        void SuppliserManagement()
+        {
+
+        }
+        void MerchandiseInfo()
+        {
+
+        }
+        void InventoryManagement()
+        {
+
+        }
+        #endregion
+
+
     }
 }
